1db983d8fdfb175049fb46929db7aac42131e539
Magno Fernando Brixner Biét <magnobiet@users.noreply.github.com>
2019-04-16 17:05:10 -0300

    Sort indications by name

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -9,7 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
 		created() {
 
 			fetch('https://my-json-server.typicode.com/magnobiet/indications/categories?_embed=indications').then((response) => response.json()).then((response) => {
-				this.categories = response;
+
+				this.categories = response.map((category) => {
+
+					category.indications.sort((a, b) => {
+
+						if (a.title > b.title) {
+							return 1;
+						}
+
+						if (a.title < b.title) {
+							return -1;
+						}
+
+						return 0;
+
+					});
+
+					return category;
+
+				});
+
 			});
 
 		}
